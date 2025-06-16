09a7749a3890e2ccd87ab4fd50c8f77a0e53a3d8
Kevin29000 <kcorre444@gmail.com>
2025-06-16 09:07:41 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const createError = require('http-errors');
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const { connectToMongo } = require('./database/db');
+
+const apiRouter = require('./api/ticket-api');
+
+const authRouter = require('./routes/auth');
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/user');
+const ticketRouter = require('./routes/ticket');
+
+const app = express();
+
+// connexion à la DB
+connectToMongo();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'secret-key',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Rend le name disponible dans tous les templates
+app.use((req, res, next) => {
+  res.locals.user = req.session.user || null;
+  next();
+});
+
+app.use('/api', apiRouter);
+
+app.use('/auth', authRouter);
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+app.use('/ticket', ticketRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// router.get("/", (req, res) => {
+//   // res.render("home");
+//   const allTickets = ticketService.getAllTickets();
+  
+//   res.render("ticket-list", { tickets: allTickets });
+// });
+
+module.exports = router;
